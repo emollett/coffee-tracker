81325e2d48c7c08f05592679fc7f66a0f0b69bbe
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-01-14 13:25:44 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git pull origin master 
+git pull origin master; 
 rm -rf coffee-tracker-backend/build;
 cd coffee-tracker-frontend;
 npm install; # install in case any new front end deps have been included since last install

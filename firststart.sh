2b100d30d5e970daf7794c2a6d4bc553ce617127
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-03-04 10:46:33 +0000

    Update firststart.sh

--- firststart.sh
+++ firststart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git pull origin master; 
 rm -rf coffee-tracker-backend/build;
 cd coffee-tracker-frontend;
 npm install; # install in case any new front end deps have been included since last install

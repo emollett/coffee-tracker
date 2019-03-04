e4072412c7f3bac7bc661f60f3cb0079af4f9cc9
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-03-04 11:03:26 +0000

    build folder added

---
+++ coffee-tracker-backend/build/precache-manifest.56670d5b10c56b2ad29f61bd520de666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3ddc517a85d84dd1f60",
+    "url": "/static/js/main.f3ddc517.chunk.js"
+  },
+  {
+    "revision": "cbf1170a4aacf0c53289",
+    "url": "/static/js/1.cbf1170a.chunk.js"
+  },
+  {
+    "revision": "f3ddc517a85d84dd1f60",
+    "url": "/static/css/main.967f8615.chunk.css"
+  },
+  {
+    "revision": "f86761f58a97771e241d535d173a5e15",
+    "url": "/index.html"
+  }
+];

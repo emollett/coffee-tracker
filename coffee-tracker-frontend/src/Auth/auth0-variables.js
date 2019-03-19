cea6edea98f2de1f9143054d4514c585fdba3611
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-03-19 11:52:27 +0000

    updated Auth config file for live

--- coffee-tracker-frontend/src/Auth/auth0-variables.js
+++ coffee-tracker-frontend/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'coffee-tracker.eu.auth0.com',
   clientId: 'Dn61gamMNP0DhK5410emcTc4BholdXGc',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'coffee-tracker.eleanormollett.com/callback'
 }

42e239b575d41840db0307e180d0fe8e0ce36492
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-03-29 13:42:24 +0000

    updated start script with new process

--- firststart.sh
+++ firststart.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
+git pull origin master;
 rm -rf coffee-tracker-backend/build;
-cd coffee-tracker-frontend;
-npm install; # install in case any new front end deps have been included since last install
-npm run build; 
-cd ..;
-cp -R coffee-tracker-frontend/build/ coffee-tracker-backend;
+aws s3 cp --recursive s3://coffee-tracker/coffee-tracker/coffee-tracker-frontend/ coffee-tracker-backend;
 cd coffee-tracker-backend;
 npm install; # install in case any new back end deps have been included since last install
 sudo service mongod start;

2cd388cf336415e0dd9bda7af10d1f2c74a225fd
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-04-05 16:04:04 +0100

    updated run scriptwith new s3 route

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 git pull origin master;
 rm -rf coffee-tracker-backend/build;
-aws s3 cp --recursive s3://coffee-tracker/coffee-tracker/coffee-tracker-frontend/ coffee-tracker-backend;
+aws s3 cp --recursive s3://coffee-tracker/coffee-tracker-frontend/ coffee-tracker-backend;
 cd coffee-tracker-backend;
 npm install; # install in case any new back end deps have been included since last install
-sudo pm2 restart all;
+# sudo pm2 restart all;

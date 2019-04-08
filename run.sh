bc23fdf5370232f625f053ea807100d12d359012
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-04-08 15:53:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ rm -rf coffee-tracker-backend/build;
 aws s3 cp --recursive s3://coffee-tracker/coffee-tracker-frontend/ coffee-tracker-backend;
 cd coffee-tracker-backend;
 npm install; # install in case any new back end deps have been included since last install
-# sudo pm2 restart all;
+sudo pm2 restart server;

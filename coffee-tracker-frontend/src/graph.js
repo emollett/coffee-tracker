35f128eb24fdb50828607ed84fb3c422f93ba167
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-05-21 11:22:33 +0100

    When there has been no coffee added in a month, that month is added in with 0 purchases to ensure the line graph renders properly, rather than having a gap

--- coffee-tracker-frontend/src/graph.js
+++ coffee-tracker-frontend/src/graph.js
@@ -26,16 +26,32 @@ class Graph extends Component {
         if (months !== undefined) {
           months.purchased++
         }else{
-          coffeeByMonth.push({monthOpened:monthofDate.getMonth(), yearOpened:monthofDate.getYear(), purchased: 1, sort:(12*monthofDate.getYear())+(monthofDate.getMonth())-1425})
+          coffeeByMonth.push({monthOpened:monthofDate.getMonth(), yearOpened:monthofDate.getYear(), purchased: 1, sortNumber:(12*monthofDate.getYear())+(monthofDate.getMonth())-1425})
         }
-    }
+
+      }
     })
 
     coffeeByMonth.sort(function (a, b){
-      return a.sort - b.sort;
+      return a.sortNumber - b.sortNumber;
     })
 
-    let coffeeByMonthXY = coffeeByMonth.map( coffee => {return {x:coffee.sort, y:coffee.purchased}});
+    //go through coffee by month starting with the first year, first month
+    var i;
+    for (i=0; i<(coffeeByMonth.length-1); i++){
+      console.log("The month is " + coffeeByMonth[i].monthOpened);
+        //if a month is missing, insert an entry for that month with no purchases
+        if (coffeeByMonth[i].sortNumber == coffeeByMonth[i+1].sortNumber - 1){
+          console.log("and it is the same as the next month plus 1");
+        }else{
+          console.log("Oh no, we skipped one")
+          coffeeByMonth.splice(i+1, 0, {monthOpened:(coffeeByMonth[i].monthOpened + 1), yearOpened:coffeeByMonth[i].yearOpened, purchased: 0, sortNumber:(coffeeByMonth[i].sortNumber + 1)});
+          console.log("We added " + coffeeByMonth[i+1].sortNumber);
+        }
+    }
+
+
+    let coffeeByMonthXY = coffeeByMonth.map( coffee => {return {x:coffee.sortNumber, y:coffee.purchased}});
 
     console.log(coffeeByMonth);
     console.log(coffeeByMonthXY);

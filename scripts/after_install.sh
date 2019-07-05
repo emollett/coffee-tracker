215d2803da3c31c86e31948076d814c41d3053f9
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-07-05 15:26:39 +0100

    some changes to the appspec

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,6 @@
 # scripts/after_install/
 #!/bin/bash
+aws s3 cp --recursive s3://coffee-tracker/coffee-tracker-frontend/ coffee-tracker-backend;
 cd coffee-tracker-backend;
 npm install; # install in case any new back end deps have been included since last install
 sudo pm2 restart server;
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,3 @@
 # scripts/before_install
 #!/bin/bash
-cd /coffee-tracker;
-git pull origin master;
-rm -rf coffee-tracker-backend/build;
+rm -rf coffee-tracker;

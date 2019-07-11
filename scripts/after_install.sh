97e5992aab327c073097bde5ae211bcf64d831d4
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-07-11 13:11:50 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,4 +3,4 @@
 sudo aws s3 cp --recursive s3://coffee-tracker/coffee-tracker-frontend/ /ubuntu/coffee-tracker/coffee-tracker-backend;
 cd /ubuntu/coffee-tracker/coffee-tracker-backend;
 sudo npm install; # install in case any new back end deps have been included since last install
-sudo pm2 restart server;
+sudo pm2 restart API;

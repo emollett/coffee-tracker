7836b19938dc9fdacbe74493b9ed45dc5211d41b
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-04-04 15:57:51 +0100

    Switching to yarn

--- firststart.sh
+++ firststart.sh
@@ -4,6 +4,6 @@ git pull origin master;
 rm -rf coffee-tracker-backend/build;
 aws s3 cp --recursive s3://coffee-tracker/coffee-tracker/coffee-tracker-frontend/ coffee-tracker-backend;
 cd coffee-tracker-backend;
-npm install; # install in case any new back end deps have been included since last install
+yarn add; # install in case any new back end deps have been included since last install
 sudo service mongod start;
 sudo pm2 start all;
--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 sudo aws s3 cp --recursive s3://coffee-tracker/coffee-tracker-frontend/ /ubuntu/coffee-tracker/coffee-tracker-backend;
 cd /ubuntu/coffee-tracker/coffee-tracker-backend;
-sudo npm install; # install in case any new back end deps have been included since last install
+sudo yarn add; # install in case any new back end deps have been included since last install
 sudo pm2 restart API;

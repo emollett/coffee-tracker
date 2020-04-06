aca8f097d9bcfcc2a71d8362917470324f112a7f
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-04-06 17:36:47 +0100

    Fixed issue with no purchases in January  inserting invalid date

--- coffee-tracker-frontend/src/Graph/Graph.js
+++ coffee-tracker-frontend/src/Graph/Graph.js
@@ -1,98 +1,104 @@
-import React, { Component } from 'react';
-import '../App.css';
-import Moment from 'react-moment';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, MarkSeries} from 'react-vis';
+/* eslint-disable no-empty */
+import React, { Component } from "react";
+import "../App.css";
+import Moment from "react-moment";
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, MarkSeries} from "react-vis";
 import "react-vis/dist/style.css";
 
 
 class Graph extends Component {
 
 
-  render() {
+	render() {
 
-    let coffeeByMonth = [];
-    this.props.data.forEach( entry => {
+		let coffeeByMonth = [];
+		this.props.data.forEach( entry => {
 
-      var monthofDate = new Date(entry.date);
+			let dateOpened = new Date(entry.date);
 
-      if (monthofDate.getYear() === 70 || monthofDate == 'Invalid Date'){
-      }else{
+			if (dateOpened.getYear() === 70 || dateOpened == "Invalid Date"){
+			}else{
 
-        var months = coffeeByMonth.find(months => {return months.monthOpened === monthofDate.getMonth() && months.yearOpened === monthofDate.getYear()});
+				let dates = coffeeByMonth.find(dates => {return dates.monthOpened === dateOpened.getMonth() && dates.yearOpened === dateOpened.getYear();});
 
-        if (months !== undefined) {
-          months.purchased++;
-        }else{
-          coffeeByMonth.push({monthOpened: monthofDate.getMonth(),
-                              yearOpened: monthofDate.getYear(),
-                              purchased: 1,
-                              sortNumber: (12*monthofDate.getYear())+(monthofDate.getMonth()),
-                              label: monthofDate.setDate(1)
-                            });
-        }
-      }
-    })
+				if (dates !== undefined) {
+					dates.purchased++;
+				}else{
+					coffeeByMonth.push({
+						monthOpened: dateOpened.getMonth(),
+						yearOpened: dateOpened.getYear(),
+						purchased: 1,
+						sortNumber: (12*dateOpened.getYear())+(dateOpened.getMonth()),
+						label: dateOpened.setDate(1)
+					});
+				}
+			}
+		});
 
-    coffeeByMonth.sort(function (a, b){
-      return a.sortNumber - b.sortNumber;
-    })
+		coffeeByMonth.sort(function (a, b){
+			return a.sortNumber - b.sortNumber;
+		});
 
-    var monthofDate = new Date(coffeeByMonth[0].label);
+		//inserts a month with 0 purchases right at the beginning to make the graph start from 0
+		let dateOpened = new Date(coffeeByMonth[0].label);
+		coffeeByMonth.unshift({
+			monthOpened: (coffeeByMonth[0].monthOpened - 1),
+			yearOpened: coffeeByMonth[0].yearOpened,
+			purchased: 0,
+			sortNumber: (coffeeByMonth[0].sortNumber - 1),
+			label: dateOpened.setMonth(coffeeByMonth[0].monthOpened - 1)
+		});
+    
+		//if a month is missing, insert an entry for that month with no purchases
+		var i;
+		for (i=0; i<(coffeeByMonth.length-1); i++){
+			if (coffeeByMonth[i].sortNumber === coffeeByMonth[i+1].sortNumber - 1){
+			}else{
+				dateOpened = new Date(coffeeByMonth[i].label);
+				coffeeByMonth.splice(i+1, 0, {
+					monthOpened:((coffeeByMonth[i].monthOpened == 11) ? coffeeByMonth[i].monthOpened - 11 :  coffeeByMonth[i].monthOpened + 1),
+					yearOpened:((coffeeByMonth[i].monthOpened == 11) ? coffeeByMonth[i].yearOpened + 1 : coffeeByMonth[i].yearOpened),
+					purchased: 0,
+					sortNumber:(coffeeByMonth[i].sortNumber + 1),
+					label:dateOpened.setMonth(coffeeByMonth[i].monthOpened +1)
+				});
+			}
+		}
 
-    coffeeByMonth.unshift({monthOpened: (coffeeByMonth[0].monthOpened - 1),
-                          yearOpened: coffeeByMonth[0].yearOpened,
-                          purchased: 0,
-                          sortNumber: (coffeeByMonth[0].sortNumber - 1),
-                          label: monthofDate.setMonth(coffeeByMonth[0].monthOpened - 1)
-                        });
-    var i;
-    for (i=0; i<(coffeeByMonth.length-1); i++){
-      //if a month is missing, insert an entry for that month with no purchases
-      if (coffeeByMonth[i].sortNumber === coffeeByMonth[i+1].sortNumber - 1){
-      }else{
-        monthofDate = new Date(coffeeByMonth[i].label);
-
-        coffeeByMonth.splice(i+1, 0, {monthOpened:(coffeeByMonth[i].monthOpened + 1),
-                                      yearOpened:coffeeByMonth[i].yearOpened,
-                                      purchased: 0,
-                                      sortNumber:(coffeeByMonth[i].sortNumber + 1),
-                                      label:monthofDate.setMonth(coffeeByMonth[i].monthOpened +1)
-                                    });
-      }
-    }
-
-    let coffeeByMonthXY = coffeeByMonth.map( coffee => {return {x:coffee.label, y:coffee.purchased}});
+		let coffeeByMonthXY = coffeeByMonth.map( coffee => {return {x:coffee.label, y:coffee.purchased};});
 
 
-    return (
+		return (
 
-      <div className="bigPadding">
-        <ul>
-          {coffeeByMonth.length <= 0
-            ? "NO DB ENTRIES YET"
-            : coffeeByMonth.map(months => (
-                <div key={months.monthOpened}>
-                  <p><strong><Moment parse="M" format="MMMM">{months.monthOpened + 1}</Moment>/<Moment parse="YYYY" format="YYYY">{months.yearOpened + 1900}</Moment></strong> : {months.purchased}</p>
-                </div>
-              ))}
-        </ul>
+			<div className="bigPadding">
+				<h2>Graph of how much coffee is being purchased each month</h2>
+				<XYPlot
+					width={900}
+					height={300}>
+					<HorizontalGridLines />
+					<MarkSeries
+						data={coffeeByMonthXY}/>
+					<LineSeries
+						data={coffeeByMonthXY}/>
+					<XAxis xType={"time"} tickLabelAngle={-30} title="Month opened" tickTotal={5} />
+					<YAxis title="Number of packs opened"/>
+				</XYPlot>
 
-        <XYPlot
-          width={900}
-          height={300}>
-          <HorizontalGridLines />
-          <MarkSeries
-            data={coffeeByMonthXY}/>
-          <LineSeries
-            data={coffeeByMonthXY}/>
-          <XAxis xType={"time"} tickLabelAngle={-30} title="Month opened" tickTotal={5} />
-          <YAxis title="Number of packs opened"/>
-        </XYPlot>
+				<h3>Data from the graph</h3>
+				<ul>
+					{coffeeByMonth.length <= 0
+						? "NO DB ENTRIES YET"
+						: coffeeByMonth.map(dates => (
+							<div key={dates.monthOpened}>
+								<p><strong><Moment parse="M" format="MMMM">{dates.monthOpened + 1}</Moment>/<Moment parse="YYYY" format="YYYY">{dates.yearOpened + 1900}</Moment></strong> : {dates.purchased}</p>
+							</div>
+						))}
+				</ul>
 
-      </div>
+			</div>
 
-    );
-  }
+		);
+	}
 }
 
 
